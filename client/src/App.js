54bf4be3caa73f97b14fed0bf1c5dfd18c41396b
Ragalina Palaka <ragalinapalaka@gmail.com>
2023-08-15 15:11:44 -0700

    made product listing side bar collapsible

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
   return (
     <main>
--- client/src/components/ProductListingFilter/index.jsx
+++ client/src/components/ProductListingFilter/index.jsx
@@ -2,6 +2,7 @@ import "./style.css";
 import React from 'react';
 import Typography from '../../../node_modules/@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
+import Collapsible from 'react-collapsible';
 
 const ProductListingFilter = () => {
   
@@ -15,32 +16,30 @@ const ProductListingFilter = () => {
     };
     return (
         <main>
-            <div class="filter">
-                <h2 class="filter-label">Filter</h2>
+            <Collapsible trigger="Filter" transitionTime = {100} transitionCloseTime = {100}>
                 <div class="cat-names">
                     <a href="/">Sub-category 1</a>
                     <a href="/">Sub-category 2</a>
                     <a href="/">Sub-category 3</a>
                     <a href="/">Sub-category 4</a>
                 </div>
-            </div>
-            <div class="slider">
-            <h2 class="slider-label">Price Range Slider</h2>
+            </Collapsible>  
+            <br></br>
+            <Collapsible trigger="Price Range" transitionTime = {100} transitionCloseTime = {100}>
                 <div class="price-content">
                         <p>$0</p>
                         <p>$100</p>
                 </div>
-                <Typography gutterBottom>
-                </Typography>
+                <Typography gutterBottom></Typography>
                 <div class="actual-slider">
                 <Slider
-                value={value}
-                onChange={rangeSelector}
-                valueLabelDisplay="auto"
+                    value={value}
+                    onChange={rangeSelector}
+                    valueLabelDisplay="auto"
                 />
                 </div>
                 <p class="price-range">Price Range: {value[0]} to {value[1]}</p>
-            </div>
+            </Collapsible>              
         </main>
     );
   };
