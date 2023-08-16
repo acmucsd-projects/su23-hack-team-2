bbbdf033772968aa424867e50f097af9f43fbf45
Ragalina Palaka <ragalinapalaka@gmail.com>
2023-08-16 13:50:37 -0700

    merged jun's account

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import ProductListing from './components/ProductListing';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/NavBar';
+import PostListingPage from './components/Post/Post';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <Navbar/>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<ProductListing />} />
+            <Route exact path="/x" element={<PostListingPage/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </main>
   );
 }
 
