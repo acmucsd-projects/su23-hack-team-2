5ab0dde7c14cf4ba20b956e52148f4463403c389
lisasunhwang <lshwang@ucsd.edu>
2023-09-02 17:59:04 -0600

    removed .eslintcache contents and unused files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from '../src/assets/tritontrade.png';
 import './App.css';
 import AccountInfo from './components/AccountInfo';
 import Inbox from './components/Inbox'
@@ -7,11 +6,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <AccountInfo/>
       <Inbox/>
         <BrowserRouter>
           <Routes>
+            <Route exact path="/accountinfo" element={<AccountInfo />} />
             <Route exact path="/inbox" element={<Inbox />} />
-            {/* <Route exact path="/accountinfo" element={<AccountInfo />} /> */}
           </Routes>
         </BrowserRouter>
     </div>
