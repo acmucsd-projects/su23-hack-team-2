7b45bd047a246208fc32460f2cef59dd3ec769dd
tjune1 <130011651+tjune1@users.noreply.github.com>
2023-09-05 09:44:00 +0800

    Update app.js, attempt to connect frontend and backend

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const router = require('./api')
+const cors = require('cors');
 
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
