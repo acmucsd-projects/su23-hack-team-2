d5018064590f033f79667c69407994bb12c82e4f
Ragalina Palaka <ragalinapalaka@gmail.com>
2023-10-04 13:25:03 -0700

    mongo to algolia pipeline

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,11 +8,19 @@ const {
   uploadPicture
 } = require('../controllers/accountController')
 
+const fileSizeLimitInBytes = 2 * 1024 * 1024;
+const multer = require("multer");
+const storage = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+      fileSize: fileSizeLimitInBytes
+  },
+});
 const Account = require('../models/account');
 const router = express.Router();
 
 // GET all users 
-router.get('/', getAcc);
+router.get('/account', getAcc);
 
 // GET a single user 
 router.get('/:id', getAcc);
@@ -23,7 +31,9 @@ router.post('/', createAcc);
 // UPDATES a user 
 router.patch('/:id', updateAcc);
 
+{/*
 // upload image 
-router.put("/:id/image", storage.single("image"), uploadPicture) 
+router.put("/:id/image", storage.single("image"), uploadPicture)
+*/} 
 
 module.exports = router;
