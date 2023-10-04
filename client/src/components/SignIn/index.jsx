564160b74a6ec128d1b00c63fb9a712801d79410
Ragalina Palaka <ragalinapalaka@gmail.com>
2023-10-04 13:46:34 -0700

    sign-in-page

--- client/src/components/SignIn/index.jsx
+++ client/src/components/SignIn/index.jsx
@@ -9,8 +9,13 @@ const SignInPage = () => {
                 <link href="https://fonts.googleapis.com/css?family=Inria+Sans&display=swap" rel="stylesheet"/>
                 <link rel="stylesheet" href="style.css"/>
             </head>
-            <Login class = 'Login'/>
-            <img class = "imgLogo" src="https://cdn.discordapp.com/attachments/1122422275304587264/1131333980524908696/image.png?ex=652710e0&is=65149be0&hm=d13f23ce281b1ebaf35860b3019d1794477a10f866b25cbc11fa051c54d99027&"/>
+            <div class="page">
+                <img class = "imgLogo" src="https://cdn.discordapp.com/attachments/1122422275304587264/1131333980524908696/image.png?ex=652710e0&is=65149be0&hm=d13f23ce281b1ebaf35860b3019d1794477a10f866b25cbc11fa051c54d99027&"/>
+                <div class="login-side">
+                    <h2>Login Here</h2>
+                    <Login class="login-btn"/>
+                </div>
+            </div>
         </html>
         
     );
