7442374204cd1ad33a6690487250d4005f21bcd8
lisasunhwang <lshwang@ucsd.edu>
2023-10-04 13:42:19 -0700

    minor navbar issue

--- client/src/components/NewListing/index.jsx
+++ client/src/components/NewListing/index.jsx
@@ -11,30 +11,28 @@ const NewListing = () => {
             <title>Document</title>
         </head>
         <body>
-            <div class="v254_921">
-                <div class="newlistingbody">
-                    <div class="v255_1047">
-                        <span class="v255_1050">+</span>
-                    </div>
-                    <input type="text" class="titleinput" placeholder="Title:"></input>
-                    <input type="text" class="priceinput" placeholder="Price (USD):"></input>
-                    <select name="categories" class="categoriesdropdown">
-                        <option value="selection">Select a category:</option>
-                        <option value="academics">Academics</option>
-                        <option value="furniture">Furniture</option>
-                        <option value="clothing">Clothing</option>
-                        <option value="more">More</option>
-                    </select>
-                    <select name="condition" class="conditionsdropdown">
-                        <option value="selection">Select the item's condition:</option>
-                        <option value="brandnew">Brand New</option>
-                        <option value="likenew">Like New</option>
-                        <option value="good">Good</option>
-                        <option value="acceptable">Acceptable</option>
-                        <option value="For Parts/Not Working">For Parts/Not Working</option>
-                    </select>
-                    <input type="text" class="descriptioninput" placeholder="Description:"></input>
+            <div class="newlistingbody">
+                <div class="v255_1047">
+                    <span class="v255_1050">+</span>
                 </div>
+                <input type="text" class="titleinput" placeholder="Title:"></input>
+                <input type="text" class="priceinput" placeholder="Price (USD):"></input>
+                <select name="categories" class="categoriesdropdown">
+                    <option value="selection">Select a category:</option>
+                    <option value="academics">Academics</option>
+                    <option value="furniture">Furniture</option>
+                    <option value="clothing">Clothing</option>
+                    <option value="more">More</option>
+                </select>
+                <select name="condition" class="conditionsdropdown">
+                    <option value="selection">Select the item's condition:</option>
+                    <option value="brandnew">Brand New</option>
+                    <option value="likenew">Like New</option>
+                    <option value="good">Good</option>
+                    <option value="acceptable">Acceptable</option>
+                    <option value="For Parts/Not Working">For Parts/Not Working</option>
+                </select>
+                <input type="text" class="descriptioninput" placeholder="Description:"></input>
             </div>
         </body>
     </html>
