1f689d96b5b3f5787af9433d3546029d2dea5a42
lisasunhwang <lshwang@ucsd.edu>
2023-10-04 13:25:57 -0700

    fixed minor spacing

--- client/src/components/NewListing/index.jsx
+++ client/src/components/NewListing/index.jsx
@@ -13,11 +13,11 @@ const NewListing = () => {
         <body>
             <div class="v254_921">
                 <div class="newlistingbody">
-                    <div class="v255_1047"></div>
-                    <span class="v255_1050">+</span>
+                    <div class="v255_1047">
+                        <span class="v255_1050">+</span>
+                    </div>
                     <input type="text" class="titleinput" placeholder="Title:"></input>
                     <input type="text" class="priceinput" placeholder="Price (USD):"></input>
-
                     <select name="categories" class="categoriesdropdown">
                         <option value="selection">Select a category:</option>
                         <option value="academics">Academics</option>
@@ -33,49 +33,6 @@ const NewListing = () => {
                         <option value="acceptable">Acceptable</option>
                         <option value="For Parts/Not Working">For Parts/Not Working</option>
                     </select>
-                    {/* <div class="categorydropdown"> 
-                        <button class="categorydropdown-btn" aria-haspopup="menu">
-                            <span class="categorydropdowntitle">Category:</span>
-                            <span class="categorydropdownarrow"></span>
-                        </button>
-                        <ul class="categorydropdown-content" role="menu">
-                            <li>
-                                <a href="#">Academics</a>
-                            </li>
-                            <li>
-                                <a href="#">Furniture</a>
-                            </li>
-                            <li>
-                                <a href="#">Clothing</a>
-                            </li>
-                            <li>
-                                <a href="#">More</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="conditiondropdown"> 
-                        <button class="conditiondropdown-btn" aria-haspopup="menu">
-                            <span class="conditiondropdowntitle">Condition:</span>
-                            <span class="conditiondropdownarrow"></span>
-                        </button>
-                        <ul class="conditiondropdown-content" role="menu">
-                            <li>
-                                <a href="#">New</a>
-                            </li>
-                            <li>
-                                <a href="#">Used - Like New</a>
-                            </li>
-                            <li>
-                                <a href="#">Used - Pretty Good</a>
-                            </li>
-                            <li>
-                                <a href="#">Used - Fair</a>
-                            </li>
-                            <li>
-                                <a href="#">Used - Poor</a>
-                            </li>
-                        </ul>
-                    </div> */}
                     <input type="text" class="descriptioninput" placeholder="Description:"></input>
                 </div>
             </div>
