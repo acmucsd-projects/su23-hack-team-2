baa1ba2522213f1bb04a630bfa5dfa222407ee19
lisasunhwang <lshwang@ucsd.edu>
2023-10-04 23:42:10 -0700

    merges

---
+++ node_modules/react-instantsearch-core/dist/es/lib/warn.d.ts
@@ -0,0 +1,10 @@
+type WarnCache = {
+    current: Record<string, boolean>;
+};
+export declare const warnCache: WarnCache;
+/**
+ * Logs a warning if the condition is not met.
+ * This is used to log issues in development environment only.
+ */
+export declare function warn(condition: boolean, message: string): void;
+export {};
